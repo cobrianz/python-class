76a89caa3179828e2998add906187ea33d5de6de
cobrianz <briancheuiyot.h@gmail.com>
2024-03-02 11:55:55 +0300

    commit

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+is_hot = False
+cold = True
+
+if is_hot:
+    print('Its a hot day please bring water to the yard')
+elif cold:
+    print("It's a cold day")
+else:
+    print('Its not a hot day')
+          
