1692ca144f090cb13d694206c3348f1485a2a184
cobrianz <briancheuiyot.h@gmail.com>
2024-03-02 11:40:10 +0300

    commit

---
+++ formated_strings.py
@@ -0,0 +1,7 @@
+first = "John"
+last = "Doe"
+
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder' 
+print(msg)
+
---
+++ string--fun.py
@@ -0,0 +1,12 @@
+course = "python for beginers"
+
+""" print(course.upper())
+
+print(course.find("o"))
+
+print(course.replace("for", "4"))
+
+print(len(course)) """
+
+print('python' in course)
+
