fe8677ad504460d1ff51a03457905bc4f44cdc16
cobrianz <briancheuiyot.h@gmail.com>
2024-03-12 21:59:07 +0300

    commit

--- functions.py
+++ functions.py
@@ -2,7 +2,6 @@ def greet_user(first_name, last_name):
     print(f'Hi  {first_name} {last_name}!')
     print('welcome on!')
 
-
 print('Starting')
 greet_user(first_name="Brian", last_name='Cheruiyot')
 print('finished')
